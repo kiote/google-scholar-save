4da11cd1592345e007f705afd5f22f45bbd42a5b
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2023-12-10 21:09:20 +0200

    async version

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-function saveToLibrary() {
+async function saveToLibrary() {
   const saveButtons = document.querySelectorAll('[role="button"].gs_or_sav');
-  saveButtons.forEach((button) => {
+  for (let button of saveButtons) {
     button.click();
-    // Create a new observer instance
-    let observer = new MutationObserver((mutations, obs) => {
-      let element = document.getElementById('gs_md_albl-d');
-      if (element) {
-        let labels = element.querySelectorAll('gs_lbl');
-        let doneButton = element.querySelector('#gs_lbd_apl');
+    await new Promise((resolve) => {
+      // Create a new observer instance
+      let observer = new MutationObserver((mutations, obs) => {
+        let element = document.getElementById('gs_md_albl-d');
+        if (element) {
+          let labels = element.querySelectorAll('gs_lbl');
+          let doneButton = element.querySelector('#gs_lbd_apl');
 
-        const targetContent = 'Affective LitReview';
-        
-        labels.forEach((element) => {
-          if (element.textContent.trim() === targetContent) {
-            element.click();
-          }
-        });
+          const targetContent = 'Affective LitReview';
+          
+          labels.forEach((element) => {
+            if (element.textContent.trim() === targetContent) {
+              element.click();
+            }
+          });
 
-        
-        doneButton.click();
-        console.log("SAVED");
-        obs.disconnect(); // Stop observing changes
-      }
+          doneButton.click();
+          console.log("SAVED");
+          obs.disconnect(); // Stop observing changes
+          resolve(); // Resolve the promise
+        }
+      });
+      // Start observing the document with the configured parameters
+      observer.observe(document, { childList: true, subtree: true });
     });
-    // Start observing the document with the configured parameters
-    observer.observe(document, { childList: true, subtree: true });
-  }); 
+  }
 }
 
 saveToLibrary();
