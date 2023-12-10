a9ecb18070a889378e8c4947939ef74a9d898e06
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2023-12-10 18:19:46 +0200

    Update script.js
    Some clicking around

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function clickSaveButtons() {
-  const saveButtons = document.querySelectorAll('[role="button"]');
-  saveButtons.forEach((button) => button.click());
+function saveToLibrary() {
+  // Create a new observer instance
+  let observer = new MutationObserver((mutations, obs) => {
+    let element = document.getElementById('gs_md_albl-d-bdy');
+    if (element) {
+      console.log("Element exists!");
+      obs.disconnect(); // Stop observing changes
+
+      let elements = element.querySelectorAll('*');
+      const targetContent = 'Affective LitReview';
+      
+      elements.forEach((element) => {
+        if (element.textContent.trim() === targetContent) {
+          element.click();
+        }
+      });
+    }
+  });
+  
+  // Start observing the document with the configured parameters
+  observer.observe(document, { childList: true, subtree: true });
+  
+  const saveButtons = document.querySelectorAll('[role="button"].gs_or_sav');
+  saveButtons.forEach((button) => {
+    button.click();
+  });
+
+  
 }
 
-clickSaveButtons();
+saveToLibrary();
