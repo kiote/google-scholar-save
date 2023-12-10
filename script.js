8815b4a8ea30d6963305f9dbcece4063bb67d830
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2023-12-10 20:58:20 +0200

    more clicks

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 function saveToLibrary() {
-  // Create a new observer instance
-  let observer = new MutationObserver((mutations, obs) => {
-    let element = document.getElementById('gs_md_d');
-    if (element) {
-      obs.disconnect(); // Stop observing changes
-
-      let labels = element.querySelectorAll('gs_lbl');
-      const targetContent = 'Affective LitReview';
-      
-      labels.forEach((element) => {
-        if (element.textContent.trim() === targetContent) {
-          element.click();
-        }
-      });
-
-      let doneButton = element.querySelector('gs_btn_act');
-      console.log("DONE BUTTON");
-      console.log(doneButton);
-      doneButton.click();
-    }
-  });
-  
-  // Start observing the document with the configured parameters
-  observer.observe(document, { childList: true, subtree: true });
-  
   const saveButtons = document.querySelectorAll('[role="button"].gs_or_sav');
   saveButtons.forEach((button) => {
     button.click();
-  });
+    // Create a new observer instance
+    let observer = new MutationObserver((mutations, obs) => {
+      let element = document.getElementById('gs_md_albl-d');
+      if (element) {
+        let labels = element.querySelectorAll('gs_lbl');
+        let doneButton = element.querySelector('#gs_lbd_apl');
 
-  
+        const targetContent = 'Affective LitReview';
+        
+        labels.forEach((element) => {
+          if (element.textContent.trim() === targetContent) {
+            element.click();
+          }
+        });
+
+        
+        doneButton.click();
+        console.log("SAVED");
+        obs.disconnect(); // Stop observing changes
+      }
+    });
+    // Start observing the document with the configured parameters
+    observer.observe(document, { childList: true, subtree: true });
+  }); 
 }
 
 saveToLibrary();
