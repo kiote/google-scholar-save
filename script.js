58b07639070b57230440d08c04da28123770115a
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2023-12-11 10:04:56 +0200

    do not create observer every time

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 async function saveToLibrary() {
   const saveButtons = document.querySelectorAll('[role="button"].gs_or_sav');
+
+  // Create a new observer instance
+  let observer = new MutationObserver((mutations, obs) => {
+    let element = document.getElementById('gs_md_albl-d');
+    if (element) {
+      let labels = element.querySelectorAll('gs_ldb_lbc');
+      let doneButton = element.querySelector('#gs_lbd_apl');
+      const LABEL = 'Affective LitReview';
+
+      console.log(labels);
+      labels.forEach((element) => {
+        if (element.textContent.trim() === LABEL) {
+          element.click();
+        }
+      });
+
+      doneButton.click();
+      console.log("SAVED");
+      obs.disconnect(); // Stop observing changes
+    }
+  });
+
   for (let button of saveButtons) {
     button.click();
     await new Promise((resolve) => {
-      // Create a new observer instance
-      let observer = new MutationObserver((mutations, obs) => {
-        let element = document.getElementById('gs_md_albl-d');
-        if (element) {
-          let labels = element.querySelectorAll('gs_ldb_lbc');
-          let doneButton = element.querySelector('#gs_lbd_apl');
-          const LABEL = 'Affective LitReview';
-
-          console.log(labels);
-          labels.forEach((element) => {
-            if (element.textContent.trim() === LABEL) {
-              element.click();
-            }
-          });
-
-          doneButton.click();
-          console.log("SAVED");
-          obs.disconnect(); // Stop observing changes
-          resolve(); // Resolve the promise
-        }
-      });
       // Start observing the document with the configured parameters
       observer.observe(document, { childList: true, subtree: true });
+
+      // Add a 1-second delay between button clicks
+      setTimeout(() => {
+        observer.disconnect(); // Stop observing changes
+        resolve(); // Resolve the promise
+      }, 1000);
     });
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Add a 1-second delay between button clicks
   }
 }
 
