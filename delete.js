370c231521447b3bead1d42b24eb190a5f7bff2c
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2024-01-29 16:19:17 +0200

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,1 +1,12 @@
+fuction clickDeleteButton() {
+  const deleteButton = document.querySelector('.gs_or_del.gs_or_btn'); // Select the delete button
 
+  if (deleteButton) {
+    deleteButton.click(); // Click the delete button if it exists
+    console.log("DELETED");
+  } else {
+    console.log("DELETE button not found");
+  }
+}
+
+clickDeleteButton(); // Call the function to perform the action
