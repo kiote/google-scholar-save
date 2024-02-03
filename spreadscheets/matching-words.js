6d13dbb66e9ffd30fef817ff6c49e71bbcddf280
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2024-02-03 13:20:49 +0200

    add logging

--- spreadscheets/matching-words.js
+++ spreadscheets/matching-words.js
@@ -1,13 +1,15 @@
-// removes rows with given word in a cell
-// to run it, paste it to "Extensions" > "Apps Script" and click run (save the script before)
 function deleteRows() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   var range = sheet.getDataRange();
   var values = range.getValues();
+  var deleted = 0;
   
   for(var i = values.length-1; i >= 0; i--) {
-    if(values[i].join().match(/robot/)) {
-      sheet.deleteRow(i+1);
+    if(values[i].join().match(/language/i)) {
+      // sheet.getRange(i + 1, 1, 1, sheet.getLastColumn()).setBackground("yellow");
+      sheet.deleteRow(i + 1);
+      deleted++;
     }
   }
-}
+
+  Logger.log("deleted: " + deleted);
