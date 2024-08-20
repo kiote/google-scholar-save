eb6dec38836ef153f272d7413c8cee50e6d689f5
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2024-08-20 20:05:10 +0300

    Update zotero-automerge.js

--- zotero/zotero-automerge.js
+++ zotero/zotero-automerge.js
@@ -1,7 +1,7 @@
 // found here https://forums.zotero.org/discussion/94721/merge-all 
 var DupPane = Zotero.getZoteroPanes();
-for(var i = 0; i < 100; i++) { // if you have more than 100 duplicates, increase this number
-    await new Promise(r => setTimeout(r, 1000)); // can be smaller than 1000, then it works faster
+for(var i = 0; i < 10000; i++) { // if you have more than 100 duplicates, increase this number
+    await new Promise(r => setTimeout(r, 100)); // can be smaller than 1000, then it works faster
     DupPane[0].mergeSelectedItems();
     Zotero_Duplicates_Pane.merge();
 }
